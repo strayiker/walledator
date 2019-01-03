896d71b1365c062b69aa2f7f718c733864e200fe
Kirill Mesnyankin <sadbox.games@gmail.com>
2019-01-03 17:17:09 +0300

    Move exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-export { default } from './validators';
+import Any from './validators/Any';
+import Array from './validators/Array';
+import String from './validators/String';
+import Shape from './validators/Shape';
+import Number from './validators/Number';
+import Email from './validators/Email';
+
+export default {
+  get any() {
+    return new Any();
+  },
+  get array() {
+    return new Array();
+  },
+  get string() {
+    return new String();
+  },
+  get shape() {
+    return new Shape();
+  },
+  get number() {
+    return new Number();
+  },
+  get email() {
+    return new Email();
+  },
+};
